226f49291a2ccddbf4794ee4d52e67fc9775ae6f
Roman Banakh <banakh.ri@gmail.com>
2019-03-11 19:24:19 +0200

    Added deploy_cluster script

--- .travis_scripts/deploy_cluster.py
+++ .travis_scripts/deploy_cluster.py
@@ -11,18 +11,28 @@ def stack_exists(client, stack_name):
             return True
     return False
 
-def stack_operations(client, stack_name, template, operation):
+def stack_operations(client, stack_name, template, ssh_key_name, operation):
     if operation == "create":
         with open(template, 'r') as cfn_template:
             return client.create_stack(StackName=stack_name,
                                        TemplateBody=cfn_template.read(),
-                                       )
+                                       Parameters=[
+                                           {
+                                                'ParameterKey': 'keyName',
+                                                'ParameterValue': ssh_key_name,
+                                           }
+                                        ])
     elif operation == "update":
         with open(template, 'r') as cfn_template:
             try:
                 return client.update_stack(StackName=stack_name,
                                            TemplateBody=cfn_template.read(),
-                                           )
+                                           Parameters=[
+                                           {
+                                                'ParameterKey': 'keyName',
+                                                'ParameterValue': ssh_key_name,
+                                           }
+                                        ])
             except ClientError as e:
                 print ("[Skipping stack update] {0}".format(e))
     else:
@@ -33,15 +43,16 @@ def get_arguments():
     parser = ArgumentParser(description='Check stack exists')
     parser.add_argument('--stack-name', help='CFn stack name', required=True)
     parser.add_argument('--template', help='CloudFormation template', required=True)
+    parser.add_argument('--ssh-key-name', help='ECS Cluster SSH key name', required=True)
     return parser.parse_args()
 
 def main():
     args = get_arguments()
     client = boto3.client('cloudformation')
     if stack_exists(client, args.stack_name):
-        status = stack_operations(client, args.stack_name, args.template, operation="update")
+        status = stack_operations(client, args.stack_name, args.template, args.ssh_key_name, operation="update")
     else:
-        status = stack_operations(client, args.stack_name, args.template, operation="create")
+        status = stack_operations(client, args.stack_name, args.template, args.ssh_key_name, operation="create")
     print (status)
         
 
